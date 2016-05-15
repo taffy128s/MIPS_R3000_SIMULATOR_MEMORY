8780f4ddaabb8bc7d07bf81e687158c815b2ffa9
taffy128s <taffy128s@gmail.com>
2016-05-15 16:54:59 +0800

    Calculated entries for PT and TLB.

--- simulator/cmp.cpp
+++ simulator/cmp.cpp
@@ -1,3 +1,7 @@
 #include "global.h"
 #include "cmp.h"
 
+static unsigned iPageTableEntries = 1024 / iMemoryPageSize;
+static unsigned dPageTableEntries = 1024 / dMemoryPageSize;
+static unsigned iTLBEntries = iPageTableEntries / 4;
+static unsigned dTLBEntries = dPageTableEntries / 4;
